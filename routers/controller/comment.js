9280ad35cf9c08a8b1981b12530acf3c64e1fb35
Haithamsaleh <Haitham.saleh.elq.js@tuwaig.edu.sa>
2021-12-23 16:12:20 +0300

    comment router v0.1

---
+++ routers/controller/comment.js
@@ -0,0 +1,123 @@
+const commentModel = require("./../../db/models/comment");
+const postModel = require("./../../db/models/post");
+const meetupModel = require("./../../db/models/meetup");
+const serviceModel = require("./../../db/models/service");
+
+
+const createComment = (req, res) => {
+    console.log(req.token);
+  const { userId, desc,postId } = req.body;
+  const newComent = new commentModel({
+    desc,
+    postId,
+});
+  newComent
+    .save()
+    .then((result) => {
+        postModel
+        .findByIdAndUpdate(postId, { $push : {comment: result._id}})
+        .then((result)=>{
+            res.status(201).json(result);
+        })
+     
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
+const createCommentMeetup = (req, res) => {
+    console.log(req.token);
+  const { userId, desc,meetupId } = req.body;
+  const newComent = new commentModel({
+    desc,
+    meetupId,
+});
+  newComent
+    .save()
+    .then((result) => {
+        meetupModel
+        .findByIdAndUpdate(meetupId, { $push : {comment: result._id}})
+        .then((result)=>{
+            res.status(201).json(result);
+        })
+     
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
+const createCommentService = (req, res) => {
+    console.log(req.token);
+  const { userId, desc,serviceId } = req.body;
+  const newComent = new commentModel({
+    desc,
+    serviceId,
+});
+  newComent
+    .save()
+    .then((result) => {
+        serviceModel
+        .findByIdAndUpdate(meetupId, { $push : {comment: result._id}})
+        .then((result)=>{
+            res.status(201).json(result);
+        })
+     
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
+
+
+const getComment = (req, res) => {
+  commentModel
+    .find({userId: req.token.id, isdel: false }).populate("postId","post -_id")
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
+
+const updateComment = (req, res) => {
+  const { id } = req.params;
+  const { desc } = req.body;
+
+  console.log(id);
+  commentModel
+    .findByIdAndUpdate(id, { $set: { desc } }, { new: true })
+    .exec() 
+    .then((result) => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
+
+const deletedComment = (req, res) => {
+  const { id } = req.params;
+
+  console.log(id);
+  commentModel
+    .findByIdAndRemove(id, { new: true })
+    .exec()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
+
+module.exports = {
+  createComment,
+  getComment,
+  createCommentMeetup,
+  createCommentService,
+  updateComment,
+  deletedComment,
+};
